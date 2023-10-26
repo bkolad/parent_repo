9dad160c7ba07c3874c273e06015b78288362d98
bkolad <blazejkolad@gmail.com>
2023-10-26 14:36:09 +0200

    fix

--- ci.sh
+++ ci.sh
@@ -2,6 +2,7 @@
 
 RETRIES=6
 WAIT_TIME=10
+REPO="child_repo"
 while :
 do 
      echo "$RETRIES"
@@ -17,17 +18,19 @@ do
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: token $CI_TRIGGER_ACCESS_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs"
+        "https://api.github.com/repos/bkolad/$REPO/actions/workflows/workflow.yml/runs"
     )"
 
     workflow_conclusion=$(echo $workflow_result | jq -r '.workflow_runs[0].conclusion')
-    echo $workflow_conclusion
+    echo "workflow_conclusion for ${REPO}: ${workflow_conclusion}"
 
     if [[ $workflow_conclusion == "success" ]]
     then
-        echo "Loool X"
+        echo "SUCCESS"
         break
     fi
     sleep $WAIT_TIME
     ((RETRIES--))
+
+    echo "Waiting for workflow_conclusion of ${REPO}. Remainaing RETRIES: ${RETRIES}"
 done 
