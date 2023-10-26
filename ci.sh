e18cf73ab0958a01e9ed2e2de879c4e47f7f603f
bkolad <blazejkolad@gmail.com>
2023-10-26 11:04:05 +0200

    fix

--- ci.sh
+++ ci.sh
@@ -21,7 +21,8 @@ do
 
     #workflow_conclusion=$(curl -s "https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs" | jq -r '.workflow_runs[0].conclusion')
 
-    workflow_conclusion=$$curl_command
+    echo "$curl_command"
+    workflow_conclusion=$curl_command
     echo "$workflow_conclusion"
     echo "checj"
 
