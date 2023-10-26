f774cd37584b7a239095cd45dd601443e01fc757
bkolad <blazejkolad@gmail.com>
2023-10-26 12:10:12 +0200

    fix

--- ci.sh
+++ ci.sh
@@ -14,10 +14,10 @@ do
         curl \
         -s \
         -L \
-        -H 'Accept: application/vnd.github+json' \
+        -H "Accept: application/vnd.github+json" \
         -H "Authorization: token $ACCESS_TOKEN" \
-        -H 'X-GitHub-Api-Version: 2022-11-28' \
-        'https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs'
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs"
     )"
 
     workflow_conclusion=$(echo $workflow_result | jq -r '.workflow_runs[0].conclusion')
