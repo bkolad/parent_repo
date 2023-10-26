8b5731694fa9fdc5697b3cde68790ea0031a38ca
bkolad <blazejkolad@gmail.com>
2023-10-26 10:59:04 +0200

    fix

--- ci.sh
+++ ci.sh
@@ -2,6 +2,13 @@
 
 retries=2
 wait_time=10
+
+curl_command="-L \
+  -H 'Accept: application/vnd.github+json' \
+  -H 'Authorization: Bearer ${{ secrets.G_PERSONAL }}' \
+  -H 'X-GitHub-Api-Version: 2022-11-28' \
+  'https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs'"
+
 while :
 do 
      echo "$retries"
@@ -10,7 +17,9 @@ do
         exit 1
     fi
 
-    workflow_conclusion=$(curl -s "https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs" | jq -r '.workflow_runs[0].conclusion')
+    #workflow_conclusion=$(curl -s "https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs" | jq -r '.workflow_runs[0].conclusion')
+
+    workflow_conclusion=$($curl_command | jq -r '.workflow_runs[0].conclusion')
     echo "$workflow_conclusion"
 
     if [[ $workflow_conclusion == "success1" ]]
