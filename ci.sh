2cdfd3aa053bf87692aef93ed09d5981eb1d2020
bkolad <blazejkolad@gmail.com>
2023-10-26 11:28:49 +0200

    fix

--- ci.sh
+++ ci.sh
@@ -25,6 +25,17 @@ do
 
     #workflow_conclusion=$(curl -s "https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs" | jq -r '.workflow_runs[0].conclusion')
 
+    json_reply="$(
+        curl \
+        -L \
+        -H 'Accept: application/vnd.github+json' \
+        -H "Authorization: token $ACCESS_TOKEN" \
+        -H 'X-GitHub-Api-Version: 2022-11-28' \
+        'https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs'
+    )"
+
+    echo "$json_reply"
+
 
     workflow_conclusion=$(curl -H 'Accept: application/vnd.github+json' -H 'Authorization: Bearer $ACCESS_TOKEN' -H 'X-GitHub-Api-Version: 2022-11-28' "https://api.github.com/repos/bkolad/child_repo/actions/workflows/workflow.yml/runs")
     echo "$workflow_conclusion"
